262672c2a574dd4d003615b2e5637768ad4082c9
Teodor Georgiev <sitikom2007@gmail.com>
2024-02-25 20:59:34 +0200

    Demo console application

---
+++ Invoice Demo Ver 1.0/Invoice Demo Ver 1.0/Services/Azhur_Services.cs
@@ -0,0 +1,100 @@
+﻿using Invoice_Demo_Ver_1._0.Models;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice_Demo_Ver_1._0.Services
+{
+    public static class Azhur_Services
+    {
+        public static List<Azhur> Azhur_Data = new List<Azhur>();
+        public static void GetTableData(ExcelWorksheet worksheet)
+        {
+            for (int row = 3; row <= worksheet.Dimension.End.Row; row++)
+            {
+                if (worksheet.Cells[row, 17].Value == null)
+                    break;
+                HandleObjectData(worksheet, row);
+            }
+        }
+
+        public static void HandleObjectData(ExcelWorksheet worksheet, int row)
+        {
+#pragma warning disable CS8604
+#pragma warning disable CS8600
+            string Id = worksheet.Cells[row, 17].Value.ToString();
+            string DocumentType = worksheet.Cells[row, 18].Value.ToString();
+
+            long DocumentNum = long.Parse(worksheet.Cells[row, 19].Value.ToString());
+
+            DateTime date = DateTime.Parse(worksheet.Cells[row, 20].Value.ToString());
+            DateOnly DocumentDate = DateOnly.FromDateTime(date);
+
+            string Name = worksheet.Cells[row, 21].Value.ToString();
+
+            Decimal NoTax = Decimal.Parse(worksheet.Cells[row, 22].Value.ToString());
+            Decimal TaxBase = Decimal.Parse(worksheet.Cells[row, 23].Value.ToString());
+            Decimal VAT_Base = Decimal.Parse(worksheet.Cells[row, 24].Value.ToString());
+            
+            string Article = worksheet.Cells[row, 25].Value.ToString();
+
+            Azhur_Data.Add(new Azhur(Id, DocumentType, DocumentNum, DocumentDate, Name, NoTax, TaxBase, VAT_Base, Article));
+#pragma warning restore CS8604
+#pragma warning restore CS8600
+        }
+
+        public static void WriteAzhurTable(ExcelWorksheet worksheet)
+        {
+            int row = 2;
+
+            foreach (var AzhurObject in Azhur_Data)
+            {
+                PrintObjectData(worksheet, AzhurObject, row, 17);
+                row++;
+            }
+        }
+
+        public static void Missing(ExcelWorksheet worksheet)
+        {
+            var missing = Azhur_Data.Where(a => !NRA_Services.NRA_Data.Any(n => n.DocumentNum == a.DocumentNum)).ToList();
+
+            int row = 2;
+            foreach (var document in missing)
+            {
+                PrintObjectData(worksheet, document, row, 1);
+                row++;
+            }
+        }
+
+        public static void PrintObjectData(ExcelWorksheet worksheet, Azhur document, int row, int col)
+        {
+            worksheet.Cells[row, col++].Value = document.Id;
+            worksheet.Cells[row, col++].Value = document.DocumentType;
+            worksheet.Cells[row, col++].Value = document.DocumentNum;
+            worksheet.Cells[row, col++].Value = document.DocumentDate;
+            worksheet.Cells[row, col++].Value = document.Name;
+            worksheet.Cells[row, col++].Value = document.NoTax;
+            worksheet.Cells[row, col++].Value = document.TaxBase;
+            worksheet.Cells[row, col++].Value = document.VAT_Base;
+            worksheet.Cells[row, col++].Value = document.Article;
+
+            row++;
+        }
+
+        public static void PrintHeader(ExcelWorksheet worksheet, int col)
+        {
+            worksheet.Cells[1, col++].Value = "ИН по ДДС на контрагента";
+            worksheet.Cells[1, col++].Value = "Вид на документа";
+            worksheet.Cells[1, col++].Value = "Номер на документа";
+            worksheet.Cells[1, col++].Value = "Дата на документа";
+            worksheet.Cells[1, col++].Value = "Име на контрагента";
+            worksheet.Cells[1, col++].Value = "БЕЗ";
+            worksheet.Cells[1, col++].Value = "ДО";
+            worksheet.Cells[1, col++].Value = "ДДС";
+            worksheet.Cells[1, col++].Value = "Счетов. статия от-до/месец";
+        }
+    }
+}
---
+++ Invoice Demo Ver 1.0/Invoice Demo Ver 1.0/Services/Write_Services.cs
@@ -0,0 +1,107 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice_Demo_Ver_1._0.Services
+{
+    public static class Write_Services
+    {
+        public static void WriteExcelWorkbook()
+        {
+            using (var package = new ExcelPackage())
+            {
+                OriginalWorksheet(package);
+                MissingNRAWorksheet(package);
+                MissingAzhurWorksheet(package);
+                WrongVATWorksheet(package);
+                CancelledDocumentsWorksheet(package);
+
+                File.WriteAllBytes(GetOutputFilePath(), package.GetAsByteArray());
+            }
+        }
+
+        public static void OriginalWorksheet(ExcelPackage package)
+        {
+            ExcelWorksheet original_Worksheet = package.Workbook.Worksheets.Add("Оригинална таблица");
+            NRA_Services.PrintHeader(original_Worksheet, 1);
+            NRA_Services.WriteNRATable(original_Worksheet);
+            Azhur_Services.PrintHeader(original_Worksheet, 17);
+            Azhur_Services.WriteAzhurTable(original_Worksheet);
+        }
+
+        public static void MissingNRAWorksheet(ExcelPackage package)
+        {
+            ExcelWorksheet missingNRA = package.Workbook.Worksheets.Add("Липсващи от НАП");
+            NRA_Services.PrintHeader(missingNRA, 1);
+            NRA_Services.Missing(missingNRA);
+        }
+
+        public static void MissingAzhurWorksheet(ExcelPackage package)
+        {
+            ExcelWorksheet missingAzhur = package.Workbook.Worksheets.Add("Липсващи от Ажур");
+            Azhur_Services.PrintHeader(missingAzhur, 1);
+            Azhur_Services.Missing(missingAzhur);
+        }
+
+        public static void WrongVATWorksheet(ExcelPackage package)
+        {
+            ExcelWorksheet wrongVAT = package.Workbook.Worksheets.Add("Грешно ДДС");
+            NRA_Services.PrintHeader(wrongVAT, 1);
+            Azhur_Services.PrintHeader(wrongVAT, 17);
+            NRA_Services.WrongVAT(wrongVAT);
+        }
+
+        public static void CancelledDocumentsWorksheet(ExcelPackage package)
+        {
+            ExcelWorksheet cancelledDocuments = package.Workbook.Worksheets.Add("Сторнирани");
+            NRA_Services.PrintHeader(cancelledDocuments, 1);
+            Azhur_Services.PrintHeader(cancelledDocuments, 17);
+            NRA_Services.CancelledDocuments(cancelledDocuments);
+        }
+
+        public static string GetOutputFilePath()
+        {
+            string outputFilePath = Path.Combine(GetOutputDirectory(), GetOutputFileName());
+            return outputFilePath;
+        }
+
+        public static string GetOutputDirectory()
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Въведете директорията, в която искате да поставите обработения екселски файл:");
+                Console.ResetColor();
+                string? outputDirectory = Console.ReadLine();
+
+                if (outputDirectory != null)
+                {
+                    if (Directory.Exists(outputDirectory))
+                        return outputDirectory;
+
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"'{outputDirectory}' не е валидна директория.");
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Опитайте отново.");
+                }
+            }
+        }
+
+        public static string GetOutputFileName()
+        {
+            string fileName = "Invoice Comparative Analysis";
+            string currentDate = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            
+            return $"{fileName} {currentDate}.xlsx";
+        }
+    }
+}
